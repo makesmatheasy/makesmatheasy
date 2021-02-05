422cdf86cb812e6b67eb3095ffb6665962dab1f4
sairish2001 <sairish2001@gmail.com>
2021-02-05 20:32:57 +0530

    Single bar and KaTex Output for LCM,HCF,Factors

---
+++ appjs/displaymath.js
@@ -0,0 +1,181 @@
+function dint() {
+    var lowerli = document.getElementById("lowerlimit").value;
+    var upperli = document.getElementById("upperlimit").value;
+    if (integralvar == '') {
+        integralvar = 'x';
+    }
+    var value = document.getElementById('inputintegral').value;
+    var x = nerdamer(value);
+    var value = x.toTeX()
+    if (checkit == '' || checkit == 'notok') {
+        lowerli = '';
+        upperli = '';
+    } else {
+        if (lowerli == '' && upperli == '') {
+            lowerli = '-\\infty';
+            upperli = '\\infty';
+        } else if (lowerli != '' && upperli == '') {
+            upperli = '\\infty';
+        } else if (lowerli == '' && upperli != '') {
+            lowerli = '-\\infty';
+        }
+    }
+    katex.render('\\int_{' + lowerli + '}^{' + upperli + '}' + value + 'd' + integralvar, document.getElementById('resultintegration'), {
+        throwOnError: false
+    });
+}
+
+function sint() {
+    var lowerli = document.getElementById("lowerlimit").value;
+    var upperli = document.getElementById("upperlimit").value;
+    if (integralvar == '') {
+        integralvar = 'x';
+    }
+    if (checkit == '' || checkit == 'notok') {
+        var t = nerdamer.integrate(document.getElementById('inputintegral').value, integralvar)
+        convertkatex(document.getElementById('resultintegration'), t);
+        return t.toString();
+    } else {
+        var t = nerdamer.defint(document.getElementById('inputintegral').value, lowerli, upperli, integralvar);
+        if (lowerli == '') {
+            lowerli = "-Infinity";
+        }
+        if (upperli == '') {
+            upperli = "Infinity";
+        }
+        convertkatex(document.getElementById('resultintegration'), t);
+        return t.toString();
+    }
+}
+
+function ddiff() {
+    var value = document.getElementById('inputdifferentiatequation').value;
+    var x = nerdamer(value);
+    var value = x.toTeX();
+    var o = '';
+    if (difforder == "") {
+        difforder = '1';
+    }
+    if (diffvariable == "") {
+        diffvariable = "x";
+    }
+    if ((diffvariable == "" && difforder == "")) {
+        o = "\\dfrac{d}{dx}";
+    } else if (diffvariable == "" && difforder != "") {
+        o = '\\dfrac{d^' + difforder + '}{dx' + difforder + '}';
+    } else {
+        o = '\\dfrac{d^' + difforder + '}{d' + diffvariable + '^' + difforder + '}';
+    }
+    katex.render(o + value, document.getElementById('resultdiff'), {
+        throwOnError: false
+    });
+}
+
+function sdiff() {
+    var t = String(nerdamer.diff(document.getElementById('inputdifferentiatequation').value, diffvariable, difforder))
+    convertkatex(document.getElementById('resultdiff'), t);
+    return t;
+}
+
+function dpardiff() {
+    var value = document.getElementById('inputpartialdiff').value;
+    getparorder(document.getElementById('inputpartialorder').value);
+    var sum = 0;
+    var or = pardifforder.match(/\d+/g);
+    var po = '';
+    if (or == null) {
+        if (pardifforder == "") {
+            po = "\\frac{\\partial }{\\partial x}"
+        } else if (pardifforder != "") {
+            var sp = pardifforder.split(",")
+            var v;
+            var temp = '';
+            for (v of sp) {
+                temp += "\\partial " + v;
+            }
+            po = '\\frac{\\partial }{' + temp + '}';
+        }
+    } else {
+        for (i of or) {
+            sum += parseInt(i);
+        }
+        if (pardifforder == "") {
+            po = '\\frac{\\partial^' + sum + '}{\\partial x}'
+        } else if (pardifforder != "") {
+            var sp = pardifforder.split(",")
+            var v;
+            var temp = '';
+            for (v of sp) {
+                temp += "\\partial " + v;
+            }
+            po = '\\frac{\\partial^' + sum + '}{' + temp + '}';
+        }
+
+    }
+
+    var x = nerdamer(value);
+    value = x.toTeX();
+    katex.render(po + value, document.getElementById('resultpardiff'), {
+        throwOnError: false
+    });
+}
+
+//function displaymatrix(ar,elid){
+//        temp='\\begin{bmatrix}'
+//        for (i of ar){
+//            for(j of i){
+//                temp+=j+"&"
+//            }
+//            temp=temp.slice(0, -1);
+//            temp+='\\\\';
+//        }
+//        temp+='\\end{bmatrix}'
+//        katex.render(temp, document.getElementById(elid), {
+//            throwOnError: false
+//        });
+//}
+
+function dlap() {
+    var value = document.getElementById('inputlaplace').value;
+    value = nerdamer(value).toTeX();
+    var b = "\\mathcal{L}(";
+    a = ")";
+    katex.render(b + value + a, document.getElementById('resultlaplace'), {
+        throwOnError: false
+    });
+}
+
+function slap() {
+    var value = nerdamer.laplace(document.getElementById('inputlaplace').value, "t", "s")
+    var t = value;
+    value = nerdamer(value).toTeX();
+    katex.render(value, document.getElementById('resultlaplace'), {
+        throwOnError: false
+    });
+    return t.toString();
+}
+
+function dinvlap() {
+    var value = document.getElementById('inputinverselaplace').value;
+    value = nerdamer(value).toTeX();
+    var b = "\\mathcal{L}(";
+    a = ")";
+    katex.render(b + value + a, document.getElementById('resultinverselaplace'), {
+        throwOnError: false
+    });
+}
+
+function sinvlap() {
+    var value = nerdamer.ilt(document.getElementById('inputinverselaplace').value, "s", "t")
+    value = nerdamer(value).toTeX();
+    katex.render(value, document.getElementById('resultinverselaplace'), {
+        throwOnError: false
+    });
+}
+
+
+
+
+
+
+
---
+++ appjs/speechrecoforall.js
@@ -0,0 +1,37 @@
+var SpeechRecognitionforall = SpeechRecognition || webkitSpeechRecognition;
+var recognitionforall = new SpeechRecognition();
+
+function runSpeechRecognitionforall(output, logo) {
+    count = 0;
+    var output = document.getElementById(output);
+//		        var action = document.getElementById(input);
+    recognitionforall.onstart = function () {
+        var c = 0;
+//                    action.value = "Listening....";
+        document.getElementById(logo).src = "listening.gif";
+        document.getElementById(logo).style.height = "20px";
+        document.getElementById(logo).style.width = "20px";
+    };
+    recognitionforall.onspeechend = function () {
+        document.getElementById(logo).src = "speechlogo.png";
+        document.getElementById(logo).style.height = "25px";
+        document.getElementById(logo).style.width = "17.41px";
+        recognition.stop();
+    }
+    recognitionforall.onresult = function (event) {
+        var transcript = event.results[0][0].transcript;
+//                    if(transcript.includes("multiply")){
+//                        transcript=transcript.replace(/multiply/g, "*");
+//                    }
+        output.value = transcript;
+    };
+    try {
+        recognitionforall.start();
+    } catch (e) {
+        recognitionforall.stop();
+        document.getElementById(logo).src = "speechlogo.png";
+//                    document.getElementById('txt').value='';
+        document.getElementById(logo).style.height = "25px";
+        document.getElementById(logo).style.width = "17.41px"
+    }
+}
