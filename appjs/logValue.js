f58454ecbdcec4b97940fb706b1e774575f4c24e
nidhivanjare <rnidhivanjare@gmail.com>
2021-03-17 16:40:30 +0530

    Calculate the log

--- appjs/logValue.js
+++ appjs/logValue.js
@@ -2,3 +2,13 @@
 function display_log(){
     document.getElementById('log_values').style.display = "block";
 }
+
+
+function cal_log(){
+
+    var number1 = parseInt(document.getElementById('num').value);
+    var base = parseInt(document.getElementById('base').value);
+
+    var ans =  Math.log(number1)/Math.log(base);
+    document.getElementById('answer').innerHTML = ans;
+}
