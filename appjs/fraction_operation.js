9c78510b9b6bdf3dfcd57f1b8c05f161be95d4f2
nidhivanjare <rnidhivanjare@gmail.com>
2021-03-21 18:14:59 +0530

    layout for each operation
    -> index file
    -> fuctions in js file

--- appjs/fraction_operation.js
+++ appjs/fraction_operation.js
@@ -1,3 +1,32 @@
 function display_fractions(){
     document.getElementById('fractions').style.display = 'block';
 }
+
+function additon(){
+
+    var n = parseInt(document.getElementById('add_num').value);
+    var d = parseInt(document.getElementById('sub_den').value);
+
+    var ans = 
+
+}
+
+function subtractiom(){
+    
+}
+
+function division(){
+    
+}
+
+function Multiplication(){
+    
+}
+
+function mixed_to_improper(){
+    
+}
+
+function improper_to_mixed(){
+    
+}
