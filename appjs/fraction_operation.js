989c83b88f06b2446ad9138a86af2c40cac804c2
nidhivanjare <rnidhivanjare@gmail.com>
2021-03-22 20:52:29 +0530

    addition and subtraction

--- appjs/fraction_operation.js
+++ appjs/fraction_operation.js
@@ -4,14 +4,75 @@ function display_fractions(){
 
 function additon(){
 
-    var n = parseInt(document.getElementById('add_num').value);
-    var d = parseInt(document.getElementById('sub_den').value);
+    var n1 = parseInt(document.getElementById('one_add').value);
+    var d1 = parseInt(document.getElementById('two_add').value);
+    var n2 = parseInt(document.getElementById('three_add').value);
+    var d2 = parseInt(document.getElementById('four_add').value);
 
-    var ans = 
+    document.getElementById('a').innerHTML =n1;
+    document.getElementById('b').innerHTML = d1;
+    document.getElementById('c').innerHTML = n2;
+    document.getElementById('d').innerHTML = d2;
+
+    document.getElementById('a1').innerHTML = n1;
+    document.getElementById('b1').innerHTML = d1;
+    document.getElementById('c1').innerHTML = n2;
+    document.getElementById('d1').innerHTML = d2;
+    document.getElementById('b2').innerHTML = d1;
+    document.getElementById('d2').innerHTML = d2;
+
+
+    document.getElementById('e').innerHTML = n1*d2;
+    document.getElementById('g').innerHTML = n2*d1;
+    document.getElementById('f').innerHTML = d1*d2;
+
+    var top = (n1*d2) +(n2*d1);
+    var bot = (d1*d2);
+    document.getElementById('t').innerHTML = top;
+    document.getElementById('q').innerHTML = bot
+
+    var ans = top / bot;
+    document.getElementById('ans').innerHTML = ans;
+
+
+    
 
 }
 
 function subtractiom(){
+
+    var n1 = parseInt(document.getElementById('one_sub').value);
+    var d1 = parseInt(document.getElementById('two_sub').value);
+    var n2 = parseInt(document.getElementById('three_sub').value);
+    var d2 = parseInt(document.getElementById('four_sub').value);
+
+    document.getElementById('suba').innerHTML =n1;
+    document.getElementById('subb').innerHTML = d1;
+    document.getElementById('subc').innerHTML = n2;
+    document.getElementById('subd').innerHTML = d2;
+
+    document.getElementById('suba1').innerHTML = n1;
+    document.getElementById('subb1').innerHTML = d1;
+    document.getElementById('subc1').innerHTML = n2;
+    document.getElementById('subd1').innerHTML = d2;
+    document.getElementById('subb2').innerHTML = d1;
+    document.getElementById('subd2').innerHTML = d2;
+
+
+    document.getElementById('sube').innerHTML = n1*d2;
+    document.getElementById('subg').innerHTML = n2*d1;
+    document.getElementById('subf').innerHTML = d1*d2;
+
+    var top = (n1*d2) - (n2*d1);
+    var bot = (d1*d2);
+    document.getElementById('subt').innerHTML = top;
+    document.getElementById('subq').innerHTML = bot
+
+    var ans = top / bot;
+    document.getElementById('subans').innerHTML = ans;
+
+
+    
     
 }
 
