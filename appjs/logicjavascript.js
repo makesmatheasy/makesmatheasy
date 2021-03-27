8e4eba219b8ea79f55658d04e1b7118d3c44a20c
Ansuman Behera <sonuansuman833@gmail.com>
2021-03-28 02:23:02 +0530

    Added function for 9s and 10s compliment

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -4442,6 +4442,27 @@ function onetwoCalc() {
 		  result.innerHTML ="Binary numbers can only have 0's and 1's";
 	  
 }
+//
+function ninetenCalc(){
+  const input = document.getElementById("ninetennumber").value;
+  let result = document.getElementById("ninetenresult");
+  var nine="";
+  var ten="";
+  for(var i=0;i<input.length;i++){
+     nine='9' - input[i];
+     ten=parseInt(nine)+1;
+  }
+  result.innerHTML ="Nine's complement of "+input+" is "+ nine+"<br>";
+  result.innerHTML ="Ten's complement of "+input+" is "+ ten+"<br>";
+
+  if(input=="")
+  {
+	  result.innerHTML ="";
+  }
+  else if(input.search(8) != -1 || input.search(9) != -1)
+		  result.innerHTML ="Binary numbers can only have 0's and 1's";
+}
+
 ////////////////////date calculator///////////
 function datecal()
 {
