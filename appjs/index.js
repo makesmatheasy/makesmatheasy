ab5f56431ef0b3e860ec676c384cab8f9b5ce1ee
Yash Khare <60147732+khareyash05@users.noreply.github.com>
2021-03-27 21:26:15 +0530

    add maxima minima (#554)

--- appjs/index.js
+++ appjs/index.js
@@ -191,6 +191,7 @@ function openit(id) {
         "#bcd",
         "#vector",
         "#diffeqn",
+        "#maxmin",
         "#tangent",
         "#probabilitycollapse",
         "#joint-probabilitycollapse",
