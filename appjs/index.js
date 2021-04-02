cd65119f301c1ab497f82a56e916db0e739d73d6
Aashna <aashna.19je0007@ece.iitism.ac.in>
2021-04-03 01:43:12 +0530

    3-D Geometry

--- appjs/index.js
+++ appjs/index.js
@@ -181,7 +181,7 @@ function openit(id) {
         "#bitwise-calc",
         "#adding-all",
 		"#onetwocom-calc",
-
+        "#3dgeo",
         "#ninetencom-calc",
         "#straightline",
         "#octal-binary",
