5f79b96d27c59add170fcd76f4cd88b2a7807b50
SarthakKeshari <55992140+SarthakKeshari@users.noreply.github.com>
2021-04-06 09:41:41 +0530

    ENHANCED - Show steps for calculation in 15s and 16s complement (#841)
    * Rectified negative values input in SI-CI calculator
    
    * Enhanced GP Sum
    
    * Rectified mean median mode errorneous result
    
    * Rectified error profit loss working over negative values
    
    * Currency converter negative values rectified
    
    * Working of Primality over negative values rectified
    
    * Byju's images replaced by photoshop created images
    
    * Enhanced functionality Simple and Compound Interest Calculator
    
    * Rectified negative factorial calculation
    
    * Rectified working of Mean Median Mode over negative values
    
    * Rectified working of P and C over decimal values
    
    * Rectified letter Character acceptance by ascending/descending
    
    * Rectified time negative values working
    
    * Rectified data storage size negative values working
    
    * Rectified incorrect output of 7 and 8 compliment
    
    * Rectified incorrect output of 9 and 10 compliment
    
    * Rectified mass negative values working
    
    * Added steps to 1's and 2's complement
    
    * Added steps to 7's and 8's complement
    
    * Added steps to 9's and 10's complement
    
    * Added discount to profit/loss calculator
    
    * Added steps to 15's and 16's complement

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -5527,24 +5527,34 @@ function seveneightCalc(){
 //--------------------
 //15's 16's compliment
 function fiftnsixtnCalc() {
-    const input = document.getElementById("fiftnsixtnnumber").value;
+    var input = document.getElementById("fiftnsixtnnumber").value;
     let result = document.getElementById("fiftnsixtnresult");
+    let work = document.getElementById("fiftnsixtnworking");
+    var print = "<h5 style='margin-top: 50px;'>Working of the 15's Complement -</h5> &emsp;"
     var fiftn = "";
     var sixtn = "";
     for (var i = 0; i < input.length; i++) {
+        print+="f";
         fiftn += (16 - parseInt(input[i],16)).toString(16);
 
     }
     sixtn = (parseInt(fiftn,16) + 1).toString(16);
     result.innerHTML = "Fifteen's complement of " + input + " is " + fiftn + "<br>";
     result.innerHTML += "Sixteen's complement of " + input + " is " + sixtn + "<br>";
+    
+    print+=" - "+input+"</span> = <span style='text-decoration: underline;'>"+fiftn+"</span><br>";
+    print+= "<br><h5 style='margin-top: 5px;'>Working of the 16's Complement -</h5> &emsp; 15's Complement + 1 = 16's Complement <br>&emsp; "
+    print+=fiftn+" + 1</span> = <span style='text-decoration: underline;'>"+sixtn+"</span>";
+    work.innerHTML = print;
 
 
     if (input == "") {
         result.innerHTML = "";
+        work.innerHTML = "";
     }
     if(fiftn == "NaN"){
         result.innerHTML = "Invalid Hexa Decimal Number"
+        work.innerHTML = "";
     }
 
 }
