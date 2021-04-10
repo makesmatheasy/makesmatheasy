683bd87d50bf89bb2d999743af28cda86b96ae82
Abhijeet Sinha <abhijeetsinha.smith@gmail.com>
2021-04-10 22:26:09 +0530

    fixed bug

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -4064,19 +4064,6 @@ function emical() {
     renderMathInElement(document.getElementById("emio3"));
 }
 
-// simple and compound interest
-//-----------------------------------------------------
-function simple_interest() {
-  var p, t, r, si, ci;
-  p = document.getElementById("first").value;
-  t = document.getElementById("second").value;
-  r = document.getElementById("third").value;
-  si = parseInt((p * t * r) / 100);
-  amount = p * Math.pow(1 + r / 100, t);
-  ci = amount - p;
-  document.getElementById("num").innerHTML = "Simple interest = ₹" + si;
-  document.getElementById("num1").innerHTML = "Compound interest = ₹" + ci;
-}
 
 // cost and selling price
 //-----------------------------------------------------
