4df9d1ef09b22e455ffe940d32a9cca6b1bdc64f
RaveenaBhasin <raveenabhasin15@gmail.com>
2021-04-15 20:15:19 +0530

    Distance from point and line calculator added

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -1574,6 +1574,22 @@ var midpoint2= (Y1 + Y2)/2;
     document.getElementById('mid_output').innerHTML= 'The midpoint between (' + X1 + ',' + Y1 + ') and ('+ X2 + ',' + Y2 + ') is '+ '(' + midpoint1 + ','  + midpoint2 + ')';
     
 }
+
+function dispointsolve()
+{
+    var a,b,c;
+    a=parseFloat(document.getElementById('a').value);
+    b=parseFloat(document.getElementById('b').value);
+    c=parseFloat(document.getElementById('c').value);
+    x1=parseFloat(document.getElementById('one').value);
+    y1=parseFloat(document.getElementById('two').value);
+    var explain = document.getElementById("dis_point");
+    explain.innerHTML = "\\[Distance \\space between \\space point \\space and \\space a \\space line  =\\space  \\frac{Ax1 + By1 + C}{\\sqrt{A^2+B^2}} \\] ";
+    renderMathInElement(document.getElementById("dis_point"));
+    var dis = (Math.abs(a*x1 + b*y1 +c))/(Math.sqrt(a*a + b*b )).toFixed(2);
+    document.getElementById('dis_op').innerHTML= 'The distance between (' + x1 + ',' + y1 + ') and ' + '    ' + a  + 'x' + '+' + b + 'y' + '+' + c + '=0' + '     is      ' + dis;
+    
+}
 //-----------------------------------------------------
 //shapes calculator
 function solveperisq() {
