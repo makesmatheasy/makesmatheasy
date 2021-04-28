2a09f5bf5de4f0b90f7f325c7ef4b379882148d2
SarthakKeshari <55992140+SarthakKeshari@users.noreply.github.com>
2021-04-28 14:12:11 +0530

    RECTIFIED - Inappropriate promt in LCM/HCF/Factors  (#1467)
    * Rectified negative values input in SI-CI calculator
    
    * Enhanced GP Sum
    
    * Rectified mean median mode errorneous result
    
    * Rectified error profit loss working over negative values
    
    * Currency converter negative values rectified
    
    * Working of Primality over negative values rectified
    
    * Byju's images replaced by photoshop created images
    
    * Enhanced functionality Simple and Compound Interest Calculator
    
    * Rectified negative factorial calculation
    
    * Rectified working of Mean Median Mode over negative values
    
    * Rectified working of P and C over decimal values
    
    * Rectified letter Character acceptance by ascending/descending
    
    * Rectified time negative values working
    
    * Rectified data storage size negative values working
    
    * Rectified incorrect output of 7 and 8 compliment
    
    * Rectified incorrect output of 9 and 10 compliment
    
    * Rectified mass negative values working
    
    * Added steps to 1's and 2's complement
    
    * Added steps to 7's and 8's complement
    
    * Added steps to 9's and 10's complement
    
    * Added discount to profit/loss calculator
    
    * Added steps to 15's and 16's complement
    
    * Appropriate prompt for GP
    
    * Rectified letter inputs in GP
    
    * Appropriate prompt for AP
    
    * Added amount to SI CI
    
    * Rectified incorrect answer for 15 and 16 complement
    
    * Tagline added
    
    * HCF LCM negative prompt handled

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -535,7 +535,7 @@ function hcf(input) {
 	document.getElementById("hcfprimefactor").textContent = "";
 	if (val.search(/^[0-9 ]+$/) == -1)
 	{
-		document.getElementById("hcfprimefactor").innerHTML ="Enter numbers only";
+		document.getElementById("hcfprimefactor").innerHTML ="Enter positive numbers only";
 		return;
 	}
     val = val.split(" ");
@@ -619,7 +619,7 @@ function factorselect(numid) {
     num = num.replace(/\s+$/, ""); //right trim
 	if (num.search(/^[0-9 ]+$/) == -1)
 	{
-		document.getElementById("hcfprimefactor").innerHTML ="Enter numbers only";
+		document.getElementById("hcfprimefactor").innerHTML ="Enter positive numbers only";
 		return;
 	}
     num = num.split(" ");
@@ -651,7 +651,7 @@ function lcmsol(input) {
     var num = document.getElementById(input).value;
 	if (num.search(/^[0-9 ]+$/) == -1)
 	{
-		document.getElementById("hcfprimefactor").innerHTML ="Enter numbers only";
+		document.getElementById("hcfprimefactor").innerHTML ="Enter positive numbers only";
 		return;
 	}
     num = num.split(" ");
@@ -3379,6 +3379,33 @@ function ellipsoidsolve() {
     var c = document.getElementById("inputc").value;
     var voloutput = document.getElementById("resultofvol");
     var tsaoutput = document.getElementById("resultoftsa");
+    var voltemp = "";
+    var tsatemp = "";
+    var res=((a**1.6 * b**1.6) + (b**1.6 * c**1.6 ) + (a**1.6 * c**1.6))**0.625;
+    if ((a != "") && (b != "") && (c != "")) {
+        voltemp += "\\[" + "\\frac{4}{3}" + "\\times" + "\\pi" + "\\times" + a + "\\times" + b + "\\times" + c   + "\\]";
+        voltemp += "\\[Volume \\space of \\space Ellipsoid \\space is \\space " + eval(String(4.18 * a * b* c )) + "\\]";
+        voloutput.innerHTML = voltemp;
+        tsatemp += "\\[" + 4 + "\\pi" + "(" + "\\frac{(" + a + "\\times" + b + ")^{1.6}" + "(" + b + "\\times" + c +")^{1.6}" + "(" + a + "\\times" + c + ")^{1.6}}{3}" + " )^{\\frac{1}{1.6}}"   + "\\]";
+        tsatemp += "\\[Surface \\space area \\space of \\space Ellipsoid \\space is \\space  \\]";
+        tsatemp += "\\[" + eval(String((6.343 * res).toFixed(2) ))  + "\\]";
+        tsaoutput.innerHTML = tsatemp;
+        renderMathInElement(voloutput);
+        renderMathInElement(tsaoutput);
+    } else {
+        voloutput.innerHTML = "";
+        tsaoutput.innerHTML = "";
+    }
+}
+//Traingular Prism
+function prismsolve() {
+    var length = document.getElementById("inputprismlength").value;
+    var breadth = document.getElementById("inputprismbreadth").value;
+    var height = document.getElementById("inputprismheight").value;
+    var side = document.getElementById("inputprismside").value;
+    var voloutput = document.getElementById("resultofvolprism");
+    var tsaoutput = document.getElementById("resultoftsaprism");
+
     var voltemp = "";
     var tsatemp = "";
     var res=((a**1.6 * b**1.6) + (b**1.6 * c**1.6 ) + (a**1.6 * c**1.6))**0.625;
@@ -7749,11 +7776,22 @@ function fiftnsixtnCalc() {
         result.innerHTML = "Fifteen's complement of " + input + " is " + fiftn + "<br>";
         result.innerHTML += "Sixteen's complement of " + input + " is " + sixtn + "<br>";
 
+
         print+=" - "+input+"</span> = <span style='text-decoration: underline;'>"+fiftn+"</span><br>";
         print+= "<br><h5 style='margin-top: 5px;'>Working of the 16's Complement -</h5> &emsp; 15's Complement + 1 = 16's Complement <br>&emsp; "
         print+=fiftn+" + 1</span> = <span style='text-decoration: underline;'>"+sixtn+"</span>";
         work.innerHTML = print;
 
+        if (input == "") {
+            result.innerHTML = "";
+            work.innerHTML = "";
+        }
+        if(fiftn == "NaN"){
+            result.innerHTML = "Invalid Hexa Decimal Number"
+            work.innerHTML = "";
+        }
+    }
+}
 
         if (input == "") {
             result.innerHTML = "";
