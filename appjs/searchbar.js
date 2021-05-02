097ad81c8ef9eebbb083082647cbe8e61cc86b83
RaveenaBhasin <raveenabhasin15@gmail.com>
2021-05-02 17:30:36 +0530

    Duplicate sections removed

--- appjs/searchbar.js
+++ appjs/searchbar.js
@@ -620,6 +620,12 @@ function autocomplete(inp, arr) {
                     else if (inp.value.toUpperCase() == "LIMITS") {
                         $("#limitscollapse").slideToggle(); 
                     }
+                    else if(inp.value.toUpperCase() == "PERFECT SQUARES & CUBES IN A RANGE"){
+                        document.getElementById('squaresRanges').click(); 
+                    }
+                    else if (inp.value.toUpperCase() == "PERFECT SQUARES & CUBES IN A RANGE") {
+                        $("#squaresRanges").slideToggle(); 
+                    }
                     else if(inp.value.toUpperCase() == "3-D GEOMETRY CALCULATOR"){
                         document.getElementById('3dgeocalc').click(); 
                     }
@@ -731,7 +737,7 @@ var arrayofelements =  ["Divide", "Integration", "Differentiation", "Laplace", "
                         "Trigonometric Values", "Power Series","Unit Converter" ,"Polynomial Degree","Set Calculator","GST Calculator","EMI Calculator","Strain Calculator","Stress Calculator","Hydrostatic Pressure","3-D Geometry Calculator","Leap Year","Rank Calculator","Excess-3 Code Convertor","Grey Code Convertor","Bitwise Calculator","Boolean Algebra","Bayes Probability Theorem","Probability Properties","Empirical Probability","OPS1 on Complex Numbers","OPS2 on Complex Numbers","Properties","Identities","3-D Geometry","3-D Shapes Calculator","Shapes Calculator",
 
 
-                        "Curve Tracing","Coordinate Systems","Coordinate Geometry Calculator","Coordinate Geometry Formula and Theorem","Mean Calculator","Statistics Formulae","Pythagorean Triplets","Profit/Loss Calculations","LCM/HCF/Factors","Binary/Hexadecimal Convertor","Tangent and Normal","Application of Derivatives","Maxima and Minima of Functions","Properties of Parallel Lines","Straight Line","Plot Angle","Plot Graph","Location of Roots",
+                        "Curve Tracing","Coordinate Systems","Coordinate Geometry Calculator","Coordinate Geometry Formula and Theorem","Mean Calculator","Statistics Formulae","Pythagorean Triplets","Profit/Loss Calculations","LCM/HCF/Factors","Binary/Hexadecimal Convertor","Tangent and Normal","Application of Derivatives","Maxima and Minima of Functions","Properties of Parallel Lines","Straight Line","Plot Angle","Plot Graph","Location of Roots","Perfect Squares & Cubes in a range",
                         "SPI/CGPA Convertor","Parabola","Log Calculator","Operations on Fractions","Mathematical Reasoning","Interest(Simple,Compound)","Cost and Selling Prices","Sets,Relations and Functions","Sets Formulas List","Properties of Matrices and Determinants","Cayley Hamilton Theorem and Diagonalization","Sum of Nterms of an Arithmetic Progression","Sum of Nterms of an Geometric Progression","Integration Formulae","Definite Integration Formulae","Differential Equations","Convergence and Divergence of Series","Hyperbolic Trigonometric Identities","General Solution of Trigonometric Equations","Solution of Triangles","Greatest Integer Function and Fractional Part","Solve for Exponents","Percentage Calculator","Standard Deviation","Mean Median Mode","Prime Numbers","Plot Bar Graph"];
                       
                         
@@ -870,6 +876,12 @@ function handleclick(value) {
     else if (inp.value.toUpperCase() == "3-D GEOMETRY CALCULATOR") {
         $("#3dgeocalc").slideToggle(); 
     }
+    else if(inp.value.toUpperCase() == "PERFECT SQUARES & CUBES IN A RANGE"){
+        document.getElementById('squaresRanges').click(); 
+    }
+    else if (inp.value.toUpperCase() == "PERFECT SQUARES & CUBES IN A RANGE") {
+        $("#squaresRanges").slideToggle(); 
+    } 
     else if(inp.value.toUpperCase() == "P/B/H TRIGO"){
         document.getElementById('simpletrignocollapse').click(); 
     }
