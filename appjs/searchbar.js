43655d29569b3f0e4b9eba0289d7a9d716b31cc4
Abhijeet Sinha <abhijeetsinha.smith@gmail.com>
2021-05-12 06:14:41 +0530

    Seachbar Updated1

--- appjs/searchbar.js
+++ appjs/searchbar.js
@@ -376,12 +376,24 @@ function autocomplete(inp, arr) {
                     }
                     else if(inp.value.toUpperCase() == "COVARIANCE CALCULATOR"){
                         document.getElementById('covs').click();
+                    }  
+                    else if (inp.value.toUpperCase() == "CLOCK ANGLE CALCULATOR") {
+                        $("#clocks").slideToggle();    
+                    }
+                    else if(inp.value.toUpperCase() == "CLOCK ANGLE CALCULATOR"){
+                        document.getElementById('clocks').click();
                     }      
                     else if (inp.value.toUpperCase() == "SKEWNESS CALCULATOR") {
                         $("#skews").slideToggle();    
                     }
                     else if(inp.value.toUpperCase() == "SKEWNESS CALCULATOR"){
                         document.getElementById('skews').click();
+                    }      
+                    else if (inp.value.toUpperCase() == "NATURAL NUMBERS") {
+                        $("#sosqsn").slideToggle();    
+                    }
+                    else if(inp.value.toUpperCase() == "NATURAL NUMBERS"){
+                        document.getElementById('sosqsn').click();
                     }      
                     else if (inp.value.toUpperCase() == "SOLVE FOR X") {
                         $("#slvxs").slideToggle();    
@@ -854,7 +866,7 @@ var arrayofelements =  ["Divide", "Integration","T test","Z test","Chi-Square te
                         "Simple Trigonometry", "Graph", "Roman to Arabic", "Arabic to Roman", "Multiply With Steps", "Roots of Equation", "Play With Equations", "Ascending Order", "Descending Order",
                         "Conversion to Words","Work And Time Calculator", "Roundoff", "LCM", "HCF","Ellipse","Circle","Expansion of Functions","Inverse Trigonometric Identities","Trigonometric Identities","P/B/H Trigo","Limits",
                         "Vector Algebra","Vector Calculus","Laplace Transforms and Properties","Hyperbola","Joint Probability","Algebraic Equations Formulas","Date","Currency Convertor","Beta Gamma Functions","Transformation of Functions","Inverse and Periodicity of Functions","Trigonometric Functions", "Operations on Sets","Euclid Geometry",
-                        "Trigonometric Values", "Power Series","Unit Converter" ,"Polynomial Degree","Set Calculator","GST Calculator","Representation by Power Series","EMI Calculator","Binomial Coefficient Calculator","Enneadecagon Calculator","Manhattan Distance calculator","Unit Rate Calculator","Hydrostatic Pressure","3-D Geometry Calculator","Leap Year","Rank Calculator","Excess-3 Code Convertor","Linear Regression Calculator","Covariance Calculator","Skewness Calculator","Solve For X","Grey Code Convertor","Bitwise Calculator","Boolean Algebra",
+                        "Trigonometric Values", "Power Series","Natural Numbers","Unit Converter" ,"Polynomial Degree","Set Calculator","GST Calculator","Representation by Power Series","EMI Calculator","Binomial Coefficient Calculator","Enneadecagon Calculator","Manhattan Distance calculator","Unit Rate Calculator","Hydrostatic Pressure","3-D Geometry Calculator","Leap Year","Rank Calculator","Excess-3 Code Convertor","Linear Regression Calculator","Covariance Calculator","Clock Angle Calculator","Skewness Calculator","Solve For X","Grey Code Convertor","Bitwise Calculator","Boolean Algebra",
                         ,"Bayes Probability Theorem","Probability Properties","Empirical Probability","OPS1 on Complex Numbers","OPS2 on Complex Numbers","Properties","Identities","3-D Geometry","3-D Shapes Calculator","Shapes Calculator",
                         "Curve Tracing","Coordinate Systems","Coordinate Geometry Calculator","Cauchy Integral,Liouville and Rouche Theorem","Coordinate Geometry Formula and Theorem","Mean Calculator","Statistics Formulae","Pythagorean Triplets","Profit/Loss Calculations","LCM/HCF/Factors","Binary/Hexadecimal Convertor","Tangent and Normal","Application of Derivatives","Maxima and Minima of Functions","Properties of Parallel Lines","Straight Line","Plot Angle","Plot Graph","Location of Roots",
                         "SPI/CGPA Convertor","Parabola","Log Calculator","Operations on Fractions","Mathematical Reasoning","Interest(Simple,Compound)","Cost and Selling Prices","Sets,Relations and Functions","Sets Formulas List","Properties of Matrices and Determinants","Cayley Hamilton Theorem and Diagonalization","Sum of Nterms of an Arithmetic Progression","Sum of Nterms of an Geometric Progression","Integration Formulae","Definite Integration Formulae","Differential Equations","Convergence and Divergence of Series","Hyperbolic Trigonometric Identities","General Solution of Trigonometric Equations","Solution of Triangles","Greatest Integer Function and Fractional Part","Solve for Exponents","Percentage Calculator","Standard Deviation","Mean Median Mode","Prime Numbers","Plot Bar Graph",
@@ -1424,11 +1436,23 @@ function handleclick(value) {
     else if (inp.value.toUpperCase() == "COVARIANCE CALCULATOR") {
         $("#covs").slideToggle(); 
     }
+    else if(inp.value.toUpperCase() == "CLOCK ANGLE CALCULATOR"){
+        document.getElementById('clocks').click(); 
+    }
+    else if (inp.value.toUpperCase() == "CLOCK ANGLE CALCULATOR") {
+        $("#clocks").slideToggle(); 
+    }
     else if(inp.value.toUpperCase() == "SKEWNESS CALCULATOR"){
         document.getElementById('skews').click(); 
     }
     else if (inp.value.toUpperCase() == "SKEWNESS CALCULATOR") {
         $("#skews").slideToggle(); 
+    }
+    else if(inp.value.toUpperCase() == "NATURAL NUMBERS"){
+        document.getElementById('sosqsn').click(); 
+    }
+    else if (inp.value.toUpperCase() == "NATURAL NUMBERS") {
+        $("#sosqsn").slideToggle(); 
     }
     else if(inp.value.toUpperCase() == "SOLVE FOR X"){
         document.getElementById('slvxs').click(); 
