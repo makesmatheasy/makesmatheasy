1b130c52f750ddf7abdbd7c4c78d45d615229beb
Ansuman Behera <sonuansuman833@gmail.com>
2021-05-13 16:53:10 +0530

    ceva and thales theorem added

--- appjs/searchbar.js
+++ appjs/searchbar.js
@@ -525,6 +525,12 @@ function autocomplete(inp, arr) {
                     else if(inp.value.toUpperCase() == "SSS TRIANGLES ANGLE CALCULATOR"){
                         document.getElementById('ssts').click();
                     }
+                    else if (inp.value.toUpperCase() == "CEVAS AND THALES THEOREM") {
+                        $("#cevtha").slideToggle();    
+                    }
+                    else if(inp.value.toUpperCase() == "CEVAS AND THALES THEOREM"){
+                        document.getElementById('cevtha').click();
+                    }
                     else if (inp.value.toUpperCase() == "SOLVE FOR X") {
                         $("#slvxs").slideToggle();    
                     }
@@ -1122,7 +1128,7 @@ var arrayofelements =  ["Divide", "Integration","T test","Z test","Chi-Square te
                         "Simple Trigonometry", "Graph", "Roman to Arabic", "Arabic to Roman", "Multiply With Steps", "Roots of Equation", "Play With Equations", "Ascending Order", "Descending Order",
                         "Conversion to Words","Work And Time Calculator", "Roundoff", "LCM", "HCF","Ellipse","Circle","Expansion of Functions","Inverse Trigonometric Identities","Trigonometric Identities","P/B/H Trigo","Limits",
                         "Vector Algebra","Vector Calculus","Sigma Notation","Laplace Transforms and Properties","Hyperbola","Joint Probability","Algebraic Equations Formulas","Date","Currency Convertor","Beta Gamma Functions","Transformation of Functions","Inverse and Periodicity of Functions","Trigonometric Functions", "Operations on Sets","Euclid Geometry",
-                        "Trigonometric Values", "Power Series","Unit Converter" ,"Polynomial Degree","Set Calculator","GST Calculator","Representation by Power Series","EMI Calculator","Binomial Coefficient Calculator","Enneadecagon Calculator","Manhattan Distance calculator","Unit Rate Calculator","Hydrostatic Pressure","3-D Geometry Calculator","Leap Year","Rank Calculator","Excess-3 Code Convertor","Linear Regression Calculator","Covariance Calculator","Skewness Calculator","Improper to Mixed Fraction","Supplementary Angle Calculator","SSS Triangles Angle Calculator","Solve For X","Grey Code Convertor","Bitwise Calculator","Boolean Algebra",
+                        "Trigonometric Values", "Power Series","Unit Converter" ,"Polynomial Degree","Set Calculator","GST Calculator","Representation by Power Series","EMI Calculator","Binomial Coefficient Calculator","Enneadecagon Calculator","Manhattan Distance calculator","Unit Rate Calculator","Hydrostatic Pressure","3-D Geometry Calculator","Leap Year","Rank Calculator","Excess-3 Code Convertor","Linear Regression Calculator","Covariance Calculator","Skewness Calculator","Improper to Mixed Fraction","Supplementary Angle Calculator","SSS Triangles Angle Calculator","Cevas and Thales Theorem","Solve For X","Grey Code Convertor","Bitwise Calculator","Boolean Algebra",
                         ,"Bayes Probability Theorem","Probability Properties","Empirical Probability","OPS1 on Complex Numbers","OPS2 on Complex Numbers","Properties","Identities","3-D Geometry","3-D Shapes Calculator","Shapes Calculator",
                         "Curve Tracing","Coordinate Systems","Coordinate Geometry Calculator","Cauchy Integral,Liouville and Rouche Theorem","Coordinate Geometry Formula and Theorem","Mean Calculator","Statistics Formulae","Pythagorean Triplets","Profit/Loss Calculations","LCM/HCF/Factors","Binary/Hexadecimal Convertor","Tangent and Normal","Application of Derivatives","Maxima and Minima of Functions","Properties of Parallel Lines","Straight Line","Plot Angle","Plot Graph","Location of Roots",
                         "SPI/CGPA Convertor","Parabola","Log Calculator","Operations on Fractions","Mathematical Reasoning","Interest(Simple,Compound)","Cost and Selling Prices","Sets,Relations and Functions","Sets Formulas List","Properties of Matrices and Determinants","Cayley Hamilton Theorem and Diagonalization","Sum of Nterms of an Arithmetic Progression","Sum of Nterms of an Geometric Progression","Integration Formulae","Definite Integration Formulae","Differential Equations","Convergence and Divergence of Series","Hyperbolic Trigonometric Identities","General Solution of Trigonometric Equations","Solution of Triangles","Greatest Integer Function and Fractional Part","Solve for Exponents","Percentage Calculator","Standard Deviation","Mean Median Mode","Prime Numbers","Plot Bar Graph",
@@ -1715,6 +1721,12 @@ function handleclick(value) {
     }
     else if (inp.value.toUpperCase() == "SSS TRIANGLE'S ANGLE CALCULATOR") {
         $("#ssts").slideToggle(); 
+    }
+    else if(inp.value.toUpperCase() == "CEVA'S AND THALES' THEOREM"){
+        document.getElementById('cevtha').click(); 
+    }
+    else if (inp.value.toUpperCase() == "CEVA'S AND THALES' THEOREM") {
+        $("#cevtha").slideToggle(); 
     }
     else if(inp.value.toUpperCase() == "SOLVE FOR X"){
         document.getElementById('slvxs').click(); 
