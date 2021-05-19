5417de7eac3324e7f031b889f534f6d1e3293d21
Yash <khareyash05@gmail.com>
2021-05-19 20:29:15 +0530

    Largest cube that can be inscribed within a right circular cylinder

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -10947,6 +10947,14 @@ function cubeinconefind(){
     document.getElementById("cubeinconeans").innerHTML = a
 }
 
+function cylinderincubefind(){
+    let a = parseInt(document.getElementById("cylinderincube").value)
+    var r = a / 2;
+    var h = a;
+    var V = (3.14 * Math.pow(r, 2) * h); 
+    document.getElementById("cylinderincubeans").innerHTML =  V;
+}
+
 function coneincubefind(){
     let a = parseInt(document.getElementById("shapeinscribedin").value)    
     let r = (a / Math.sqrt(2));
