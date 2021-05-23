d136b36ee77b2cdd404e96aba54f1c735b92eda7
Shrishti <73748606+shrishti-c@users.noreply.github.com>
2021-05-23 09:00:29 +0530

    Log Calculator modified (#3194)
    * covariance added
    
    * Linear Regression Added
    
    * conflict
    
    * Margin error
    
    * searchbar
    
    * Duplicacy removed
    
    * changed
    
    * added
    
    * checked
    
    * update
    
    * hypergeo added
    
    * geomean added
    
    * conflict
    
    * update
    
    * hypergeometric mean
    
    * updated
    
    * variance added
    
    * hypergeo std. dev
    
    * std dev poisson
    
    * search bar updated
    
    * growth rate
    
    * modified
    
    * log calculator modified

--- appjs/logValue.js
+++ appjs/logValue.js
@@ -6,15 +6,22 @@
 
  function cal_log(){
 
-     var number1 = parseInt(document.getElementById('x-number').value);
-     var base = parseInt(document.getElementById('log-base').value);
+     var number1 = document.getElementById('x-number').value;
+     var base =document.getElementById('log-base').value;
 
+     if(number1==""||base==""||isNaN(number1)||isNaN(base))
+     {
+      document.getElementById('result').innerHTML = "Please enter proper input to find log value";
+     }
+     else{
+        number1=parseInt(number1);
+        base=parseInt(base);
      if(base==1)
      {
         document.getElementById('result').innerHTML = "Base of logarithm cannot be 1 ";
      }
      else if (base==0){
-        document.getElementById('result').innerHTML = "Base of logarithm cannon be 0"
+        document.getElementById('result').innerHTML = "Base of logarithm cannot be 0"
      }
 	 else if(base<0 || number1<0)
 	 {
@@ -26,4 +33,5 @@
      document.getElementById('result').innerHTML = "log<sub>"+base+"</sub> ("+number1+") = "+ans;
 
      }
+   }
  } 
