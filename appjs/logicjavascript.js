18bf5137afa6241ba1e282528f257fc163867eaa
Ansuman Behera <sonuansuman833@gmail.com>
2021-05-23 11:46:32 +0530

    added

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -12377,23 +12377,23 @@ function hammingDistance() {
     if(x.length != y.length){
         result.innerHTML = "Error : Unequal Length ( Hamming distance can be calculated between 2 equal length of inputs )"
     } else {
-        // if(type == "Decimal"){
-        //     let val = x ^ y;
-        //     let hammDist = 0;
-        //     if(x.length == y.length){
-        //         while (val > 0) {
-        //             val &= val - 1;
-        //             hammDist++;
-        //         }
-        //         result.innerHTML = "The hamming distance between " + x + " and " + y + " is: " + hammDist;
-        //     }
-        // }else if(type == "String"){
-        //     let hammDist;
-        //     for (var i = 0; i <x.length ; i++) {
-        //         if(x.charAt(i)!=y.charAt(i))
-        //             hammDist += 1;
-        //     }
-        //     result.innerHTML = "The hamming distance between " + x + " and " + y + " is: " + hammDist;
+        if(type == "Decimal"){
+            let val = x ^ y;
+            let hammDist = 0;
+            if(x.length == y.length){
+                while (val > 0) {
+                    val &= val - 1;
+                    hammDist++;
+                }
+                result.innerHTML = "The hamming distance between " + x + " and " + y + " is: " + hammDist;
+            }
+        }else if(type == "String"){
+            let hammDist;
+            for (var i = 0; i <x.length ; i++) {
+                if(x.charAt(i)!=y.charAt(i))
+                    hammDist += 1;
+            }
+            result.innerHTML = "The hamming distance between " + x + " and " + y + " is: " + hammDist;
         }else if(type == "Hexadecimal"){
             x = parseInt(x,16).toString(10);
             y = parseInt(y,16).toString(10);
