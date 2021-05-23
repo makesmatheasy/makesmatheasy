2ef6f8695ad2d8e130915c4b0ee0e57baa9e22d6
Yash Khare <60147732+khareyash05@users.noreply.github.com>
2021-05-23 09:38:10 +0530

    Hardy-Ramanujan Theorem (#3222)

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -10543,6 +10543,30 @@ function typenum1find(){
     }
 }
 
+function typenum2find(){
+    let n = parseInt(document.getElementById("typenum2").value)
+    let count = 0;
+ 
+    if (n % 2 == 0)
+    {
+        count++;
+        while (n % 2 == 0)
+            n = n / 2;
+    }
+    for(let i = 3; i <= Math.sqrt(n); i = i + 2)
+    {
+        if (n % i == 0)
+        {
+            count++;
+            while (n % i == 0)
+                n = n / i;
+        }
+    }
+    if (n > 2)
+        count++;
+    document.getElementById("typenum1ans").innerHTML =  count;
+}    
+
 function findTriangularNumber(n)
 {
     return (n * (n + 1)) / 2;
