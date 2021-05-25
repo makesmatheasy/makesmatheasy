100baca7f154a11d2d83f65dce2cd53850f62bb8
Yash <khareyash05@gmail.com>
2021-05-25 11:40:13 +0530

    sum of the series 1-2+3-4+5-6+7…….

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -2817,6 +2817,13 @@ function impse11find(){
     document.getElementById("impse11ans").innerHTML =  -n / 2;
 }
 
+function impse12find(){
+    let n = parseInt(document.getElementById("impse16").value)
+    if (n % 2 == 1)
+        document.getElementById("impse12ans").innerHTML =  (n + 1) / 2;
+    document.getElementById("impse12ans").innerHTML =  -n / 2;
+}
+
 function find_count(ele){
     var count = 0;
     for (let i = 0; i < ele.length; i++) {
