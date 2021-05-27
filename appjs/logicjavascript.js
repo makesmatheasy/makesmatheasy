99f730be38c8d127484c010c7653a4f77b38f102
ShrutiOmer <shrutiomer1234@gmail.com>
2021-05-27 20:19:29 +0530

    added

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -14377,8 +14377,12 @@ function sccofind() {
     let r = parseInt(document.getElementById("scco1").value)
     var R = ((h * r * Math.sqrt(2)) / (h + Math.sqrt(2) * r)) / 2;                                                                            
     if(h<0 || r<0)
-       document.getElementById("sccoans").innerHTML+"\\[Input \\space values \\space cannot \\space be \\space negative.\\]"  
-    document.getElementById("sccoans").innerHTML =R;
+    {
+       document.getElementById("sccoans").innerHTML+="\\[Input \\space values \\space cannot \\space be \\space negative.\\]"  
+       renderMathInElement( document.getElementById("sccoans"));
+    }else {
+     document.getElementById("sccoans").innerHTML =R;
+    }  
 }
 
 function cubeinspherefind() {
