5613247f8a3a4f96b7f81e7eb8cbf6c7438d6414
Sonali Kushwaha <shonalikushwaha@gmail.com>
2021-05-30 13:18:55 +0530

    fixed error in binary output

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -17916,14 +17916,16 @@ function multBinDecHexOct() {
     print += "<h5>Step3 : Now multiply the decimal values found in STEP1 and STEP2</h5>";
     print += x1 + "&nbsp; X &nbsp;" + x2 + "&nbsp; = &nbsp;" + x3;
 
-    if(input1 === '1' || input1 === '0' || input2 === '1' || input2 === '0')
-    {
-        console.log(input1);
-        if (resultType === "Binary") {
+    // var checkbinary = "[01][01]+";
+        if (resultType === "Binary" && (/^[01]+$/.test(input1)  && /^[01]+$/.test(input2)) {
             result.innerHTML = "Answer in binary=" + fracDectoBinHexOct(x3, 2);
             print += "<h5>Step4 : To find the result in "+resultType+" convert the answer found in STEP3 to "+resultType+"</h5>";
             print += x3 + "->" + fracDectoBinHexOct(x3, 2);
-        } else if (resultType === "Octal") {
+        }
+        else{
+            print = "";
+        }
+        if (resultType === "Octal") {
             result.innerHTML = "Answer in Octal=" + fracDectoBinHexOct(x3, 8);
             print += "<h5>Step4 : To find the result in "+resultType+" convert the answer found in STEP3 to "+resultType+"</h5>";
             print += x3 + "->" + fracDectoBinHexOct(x3, 8);
@@ -17937,7 +17939,6 @@ function multBinDecHexOct() {
             print += x3.toString();
         }
         work.innerHTML = print;
-    }
 }
 
 //----------------------------
