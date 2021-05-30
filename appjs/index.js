9d57a0304e41420be1b4c1c0ca4e3e5e0a1b503e
Sonali Kushwaha <shonalikushwaha@gmail.com>
2021-05-30 11:57:00 +0530

    added the mistakenly deleted code

--- appjs/index.js
+++ appjs/index.js
@@ -130,6 +130,7 @@ function collapseit(openit) {
 
 function openit(id) {
     var ids = [
+        "#autonum",
         "#disnum",
         "#gammaDist",
         "#expoDist",
@@ -193,6 +194,7 @@ function openit(id) {
         "#antilog",
         "#kaps",
         "#chitest",
+        "#takttest",
         "#adams",
         "#integralcollapse",
         "#integration-idencollapse",
