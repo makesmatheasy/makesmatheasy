b8b439ae0d186d107e9e135d712305d9b8d85128
abhijeet141 <67696867+abhijeet141@users.noreply.github.com>
2021-05-31 07:02:21 +0530

    Added examples for sum of gould's sequence , Arcuin's seq , sos 1 + 22 + 33 + .. , sos 0.x + 0.xx +.. , Gijswjit sequence

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -3164,6 +3164,13 @@ function impse3find() {
     }
 }
 
+function calceximpse4find(){
+    var n = document.getElementById("impse8");
+    n.value = 10;
+
+    impse4find();
+}
+
 function impse4find() {
     var n = parseInt(document.getElementById("impse8").value)
     if (!isNaN(n)) {
@@ -3183,6 +3190,13 @@ function impse4find() {
     }
 }
 
+function calceximpse5find(){
+    var n = document.getElementById("impse9");
+    n.value = 10;
+
+    impse5find();
+}
+
 function impse5find() {
     var _n = document.getElementById("impse9").value;
     var n = parseInt(_n);
@@ -3196,6 +3210,13 @@ function impse5find() {
     }
     renderMathInElement(document.getElementById("impse5ans"));
     renderMathInElement(document.getElementById("impse5ans1"));
+}
+
+function calceximpse6find(){
+    var n = document.getElementById("impse10");
+    n.value = 10;
+
+    impse6find();
 }
 
 function impse6find() {
@@ -3214,6 +3235,15 @@ function impse6find() {
 
 }
 
+function calceximpse7find(){
+    var x = document.getElementById("impse1x");
+    var k = document.getElementById("impse11");
+    x.value = 9;
+    k.value = 20;
+
+    impse7find();
+}
+
 function impse7find() {
     var x = (document.getElementById("impse1x").value);
     var k = (document.getElementById("impse11").value);
@@ -3227,6 +3257,13 @@ function impse7find() {
     }
     renderMathInElement(document.getElementById("impse7ans"));
     renderMathInElement(document.getElementById("impse7ans1"));
+}
+
+function calceximpse8find(){
+    var n = document.getElementById("impse12");
+    n.value = 10;
+
+    impse8find();
 }
 
 function impse8find() {
