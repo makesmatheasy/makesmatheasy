502759e69883e8456cc591f5f89e61bd7e3299b2
Ansuman Behera <71064674+AB-san@users.noreply.github.com>
2021-06-01 00:43:39 +0530

    added excess-3 to bcd conversion (#5199)

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -22391,7 +22391,64 @@ function decimalTObcd(z = "") {
     return x;
 
 }
+function convertex3misc(){
+    const toBase = document.getElementById("misc-1").value;
+    var input = document.getElementById("misc-input").value;
+    let result = document.getElementById("misc-result");
 
+    if (toBase === "BCD Code"){
+        var x = "", y = "";
+        var temp = "";
+        if (input.length % 4 != 0) {
+            result.innerHTML = "Error : Invalid  input (Excess-3 Code comes in sets of nibbles(4 bits)"
+        } else {
+            x = "";
+
+            for (var i = 0; i < input.length; i++) {
+                if ((i + 1) % 4 == 0) {
+                    temp = temp + input[i]; //for 4 th value
+                    x = x + (parseInt(temp, 2) - 3).toString();
+                    print += "\\[\\rightarrow \\space " + temp + " \\space becomes \\space " + parseInt(temp, 2).toString() + "\\]";
+                    print += "\\[" + parseInt(temp, 2) + " \\space - \\space 3 \\space \\rightarrow \\space " + (parseInt(temp, 2) - 3).toString() + " \\]";
+                    temp = "";
+                } else {
+                    temp = temp + input[i];
+                }
+            }
+
+            if (input.length / 4 == x.length) {
+                input = parseInt(x).toString();
+            } else if (parseInt(x) < 0) {
+                result.innerHTML = "Error : Invalid input (decimal value of each digit cant subceed 0)";
+            } else {
+                result.innerHTML = "Error : Invalid input (decimal value of each digit cant exceed 9)";
+            }
+            
+
+            var x = "_", y = ""; temp = "";
+        for (var i = 0; i < input.length; i++) {
+            y = parseInt(input[i]).toString(2);
+            if (y.length == 1) {
+                temp = "000" + y + "_   ";
+                x = x + temp;
+            }
+            if (y.length == 2) {
+                temp = "00" + y + "_   ";
+                x = x + temp;
+            }
+            if (y.length == 3) {
+                temp = "0" + y + "_   ";
+                x = x + temp;
+            }
+            if (y.length == 4) {
+                temp = y + "_   ";
+                x = x + +temp;
+            }
+        }
+
+        result.innerHTML = "Answer -> " + x;
+    }
+}
 function convertbcdmisc(){
     const toBase = document.getElementById("bcd-misc-1").value;
     var input = document.getElementById("bcdmisc-input").value;
